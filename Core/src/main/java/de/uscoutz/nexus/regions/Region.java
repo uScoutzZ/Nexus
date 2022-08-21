c994d03156a180ab03a253f5476f589a53fbb0ea
BuildTools <unconfigured@null.spigotmc.org>
2022-08-21 13:06:41 +0200

    Add region with y Add getBlocks for regions

--- Core/src/main/java/de/uscoutz/nexus/regions/Region.java
+++ Core/src/main/java/de/uscoutz/nexus/regions/Region.java
@@ -1,22 +1,33 @@
 package de.uscoutz.nexus.regions;
 
-import net.kyori.adventure.text.Component;
-import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.entity.ArmorStand;
-import org.bukkit.entity.EntityType;
+import org.bukkit.block.Block;
 import org.bukkit.util.BoundingBox;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Region {
 
     private BoundingBox boundingBox;
+    private int minX, maxX, minY, maxY, minZ, maxZ;
+
+    public Region(int minX, int maxX, int minY, int maxY, int minZ, int maxZ) {
+        this.minX = minX;
+        this.minZ = minZ;
+        this.minY = minY;
+        this.maxX = maxX;
+        this.maxY = maxY;
+        this.maxZ = maxZ;
+    }
 
     public Region(World world, int minX, int maxX, int minZ, int maxZ, String s) {
         minX -= 1;
         minZ -= 1;
         maxX += 1;
         maxZ += 1;
-        boundingBox = new BoundingBox(minX, 0, minZ, maxX, 100, maxZ);
+        boundingBox = new BoundingBox(minX, 0, minZ, maxX, 200, maxZ);
 
         /*ArmorStand min = (ArmorStand) world.spawnEntity(new Location(world, minX, -50, minZ), EntityType.ARMOR_STAND);
         min.setCustomNameVisible(true);
@@ -27,7 +38,23 @@ public class Region {
         max.customName(Component.text("max " + s));*/
     }
 
+    public List<Block> getBlocksInRegion(World world) {
+        List<Block> blocks = new ArrayList<>();
+
+        for(int y = minY; y <= maxY; y++) {
+            for(int x = minX; x <= maxX; x++) {
+                for(int z = minZ; z <= maxZ; z++) {
+                    if(world.getBlockAt(x, y, z).getType() != Material.AIR) {
+                        blocks.add(world.getBlockAt(x, y, z));
+                    }
+                }
+            }
+        }
+
+        return blocks;
+    }
+
     public boolean overlap(int minX, int maxX, int minZ, int maxZ) {
-        return new BoundingBox(minX, 0, minZ, maxX, 100, maxZ).overlaps(boundingBox);
+        return new BoundingBox(minX, 0, minZ, maxX, 200, maxZ).overlaps(boundingBox);
     }
 }
