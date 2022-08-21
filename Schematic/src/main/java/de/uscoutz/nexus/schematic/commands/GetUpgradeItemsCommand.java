cc6bbbdba28db386640fa5f80a8ccac50ba88c61
uScoutzZ <scoutyboy1@gmail.com>
2022-08-21 14:43:56 +0200

    Add GetUpgradeItemsCommand

---
+++ Schematic/src/main/java/de/uscoutz/nexus/schematic/commands/GetUpgradeItemsCommand.java
@@ -0,0 +1,54 @@
+package de.uscoutz.nexus.schematic.commands;
+
+import de.uscoutz.nexus.NexusPlugin;
+import de.uscoutz.nexus.schematic.NexusSchematicPlugin;
+import de.uscoutz.nexus.schematic.schematics.Schematic;
+import de.uscoutz.nexus.schematic.schematics.SchematicType;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class GetUpgradeItemsCommand implements CommandExecutor {
+
+    private NexusSchematicPlugin plugin;
+
+    public GetUpgradeItemsCommand(NexusSchematicPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if(sender.hasPermission("nexus.command.getupgradeitems")) {
+            Player player = (Player) sender;
+            if(args.length >= 2) {
+                try {
+                    SchematicType schematicType = SchematicType.valueOf(args[0]);
+                    int level = Integer.parseInt(args[1]);
+                    List<ItemStack> neededItems = new ArrayList<>(plugin.getCollectorManager().getCollectorNeededMap().get(schematicType).get(level));
+
+                    for(ItemStack itemStack : neededItems) {
+                        player.getInventory().addItem(itemStack);
+                    }
+                } catch (IllegalArgumentException exception) {
+                    sendHelp(player);
+                    exception.printStackTrace();
+                }
+            } else {
+                sendHelp(player);
+            }
+        }
+
+        return false;
+    }
+
+    private void sendHelp(Player player) {
+        player.sendMessage("§cSyntax: /getupgradeitems <SchematicType> <Level>");
+    }
+}
