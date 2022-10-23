3987ef40496440aaf67f298b949dd662dd44d70b
BuildTools <unconfigured@null.spigotmc.org>
2022-10-23 13:33:45 +0200

    Finish autominer implementation

--- Schematic/src/main/java/de/uscoutz/nexus/schematic/autominer/AutoMiner.java
+++ Schematic/src/main/java/de/uscoutz/nexus/schematic/autominer/AutoMiner.java
@@ -3,10 +3,12 @@ package de.uscoutz.nexus.schematic.autominer;
 import de.uscoutz.nexus.database.DatabaseUpdate;
 import de.uscoutz.nexus.inventory.InventoryBuilder;
 import de.uscoutz.nexus.inventory.SimpleInventory;
+import de.uscoutz.nexus.item.ItemBuilder;
 import de.uscoutz.nexus.profile.Profile;
 import de.uscoutz.nexus.schematic.NexusSchematicPlugin;
 import de.uscoutz.nexus.schematic.schematics.BuiltSchematic;
 import lombok.Getter;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -42,9 +44,9 @@ public class AutoMiner {
             }
             Material type = event.getCurrentItem().getType();
             int canAdd = 0;
-            for(int i = 0; i < 3*9; i++) {
+            for(int i = 0; i < 4*9; i++) {
                 if(player.getInventory().getItem(i) == null) {
-                    canAdd = 64;
+                    canAdd += 64;
                 } else if(player.getInventory().getItem(i).getType() == type) {
                     canAdd += player.getInventory().getItem(i).getMaxStackSize() - player.getInventory().getItem(i).getAmount();
                 }
@@ -60,11 +62,12 @@ public class AutoMiner {
 
         ResultSet resultSet = plugin.getNexusPlugin().getDatabaseAdapter().getTwoAsync("autoMiners", "profileId", profile.getProfileId().toString(), "schematicId", builtSchematic.getSchematicId().toString());
         try {
-            inventory = InventoryBuilder.create(18, "AutoMiner");
+            inventory = InventoryBuilder.create(3*9, "AutoMiner");
             inventory.setDeleteOnClose(false);
             for(int i = 0; i < 18; i++) {
                 inventory.setItem(i, new ItemStack(Material.AIR), eventConsumer);
             }
+            inventory.fill(18, 27, ItemBuilder.create(Material.GRAY_STAINED_GLASS_PANE).name(" "));
             if (resultSet.next()) {
                 ItemStack[] contents = plugin.getNexusPlugin().getInventoryManager().fromBase64(resultSet.getString("inventory")).getContents();
                 for(ItemStack itemStack : contents) {
@@ -77,6 +80,24 @@ public class AutoMiner {
                 autoMinerType = AutoMinerManager.AutoMinerType.STONE;
                 plugin.getNexusPlugin().getDatabaseAdapter().setAsync("autoMiners", profile.getProfileId().toString(), builtSchematic.getSchematicId().toString(), autoMinerType.toString(), plugin.getNexusPlugin().getInventoryManager().toBase64(inventory.getInventory()));
             }
+            inventory.setItem(26, ItemBuilder.create(Material.COMPARATOR)
+                    .name(plugin.getNexusPlugin().getLocaleManager().translate("de_DE", "autominer_change-type"))
+                    .lore(plugin.getNexusPlugin().getLocaleManager().translate("de_DE", "autominer_current-type",
+                            plugin.getNexusPlugin().getLocaleManager().translate("de_DE", "autominer_type_" + autoMinerType.toString().toLowerCase()))), inventoryClickEvent -> {
+                SimpleInventory simpleInventory = InventoryBuilder.create(1*9, "AutoMiner");
+                for(AutoMinerManager.AutoMinerType minerType : AutoMinerManager.AutoMinerType.values()) {
+                    simpleInventory.addItem(ItemBuilder
+                            .create(plugin.getAutoMinerManager().getMaterialsPerType().get(minerType).get(0))
+                            .name(plugin.getNexusPlugin().getLocaleManager().translate("de_DE", "autominer_type_" + minerType.toString().toLowerCase()))
+                            .lore(plugin.getNexusPlugin().getLocaleManager().translate("de_DE", "autominer_click-to-change")), inventoryClickEvent1 -> {
+                        autoMinerType = minerType;
+                        inventoryClickEvent1.getWhoClicked().sendMessage(plugin.getNexusPlugin().getLocaleManager().translate("de_DE", "autominer_changed-type",
+                                plugin.getNexusPlugin().getLocaleManager().translate("de_DE", "autominer_type_" + autoMinerType.toString().toLowerCase())));
+                        inventoryClickEvent1.getWhoClicked().closeInventory();
+                    });
+                }
+                simpleInventory.open((Player) inventoryClickEvent.getWhoClicked());
+            });
         } catch (SQLException exception) {
             exception.printStackTrace();
         }
@@ -93,9 +114,13 @@ public class AutoMiner {
     }
 
     public void save() {
+        Inventory savedInventory = Bukkit.createInventory(null, 27);
+        for(int i = 0; i < 18; i++) {
+            savedInventory.setItem(i, inventory.getInventory().getItem(i));
+        }
         plugin.getNexusPlugin().getDatabaseAdapter().updateTwo("autoMiners", "profileId",
                 profile.getProfileId().toString(), "schematicId", builtSchematic.getSchematicId().toString(),
-                new DatabaseUpdate("inventory", plugin.getNexusPlugin().getInventoryManager().toBase64(inventory.getInventory())),
+                new DatabaseUpdate("inventory", plugin.getNexusPlugin().getInventoryManager().toBase64(savedInventory)),
                 new DatabaseUpdate("autoMinerType", autoMinerType.toString()));
     }
 }
