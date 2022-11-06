4b3cf663b003276b9146d359f98b1128f2c8c20e
uScoutzZ <scoutyboy1@gmail.com>
2022-11-06 13:02:53 +0100

    Fix bug that standard entities were removed

--- Schematic/src/main/java/de/uscoutz/nexus/schematic/listener/profile/ProfileCheckoutListener.java
+++ Schematic/src/main/java/de/uscoutz/nexus/schematic/listener/profile/ProfileCheckoutListener.java
@@ -52,7 +52,7 @@ public class ProfileCheckoutListener implements Listener {
         }
 
         for(Entity entity : profile.getWorld().getWorld().getEntities()) {
-            if(entity.getType() != EntityType.ENDER_CRYSTAL && !!profile.getWorld().getWorldEntities().contains(entity)) {
+            if(entity.getType() != EntityType.ENDER_CRYSTAL && !profile.getWorld().getWorldEntities().contains(entity)) {
                 entity.remove();
             }
         }
